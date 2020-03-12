8194f7e61e03cf93927b977fb60dd7f5345b679e
haobin12358 <1276121237@qq.com>
2020-03-12 16:43:59 +0800

    alembic

---
+++ hospital/control/CConfig.py
@@ -0,0 +1,52 @@
+"""
+本文件用于处理后台基本字段设置，包括但不限于关于我们/banner页等内容
+create user: haobin12358
+last update time:2020/3/12 15:39
+"""
+import uuid
+from flask import request, current_app
+from hospital.extensions.token_handler import admin_required
+from hospital.extensions.register_ext import db
+from hospital.extensions.params_validates import parameter_required
+from hospital.extensions.success_response import Success
+from hospital.extensions.error_response import ParamsError
+from hospital.models.config import Banner
+
+class CConfig:
+
+    @admin_required
+    def set_banner(self):
+        """banner创建/编辑/删除"""
+        data = parameter_required(('bnpicture', "bnsort",))
+        mpbid = data.get('mpbid')
+        bn_dict = {'BNpicture': data.get('bnpicture'),
+                    'BNsort': data.get('bnsort'),
+                    'contentlink': data.get('contentlink')}
+        with db.auto_commit():
+            if not mpbid:
+                """新增"""
+                bn_dict['BNid'] = str(uuid.uuid1())
+                bn_dict['ADid'] = getattr(request, 'user').id
+                bn_instance = Banner.create(bn_dict)
+                msg = '添加成功'
+            else:
+                """修改/删除"""
+                bn_instance = Banner.query.filter_by_(MPBid=mpbid).first_('未找到该轮播图信息')
+                if data.get('delete'):
+                    bn_instance.update({'isdelete': 1})
+                    msg = '删除成功'
+                else:
+                    bn_instance.update(bn_dict, null='not')
+                    msg = '编辑成功'
+            db.session.add(bn_instance)
+        return Success(message=msg, data={'mpbid': bn_instance.BNid})
+
+    def list_banner(self):
+        """小程序轮播图获取"""
+        filter_args = []
+        bn = Banner.query.filter(Banner.isdelete == False,
+                                              *filter_args
+                                              ).order_by(Banner.MPBsort.asc(),
+                                                         Banner.createtime.desc()).all()
+        [x.hide('ADid') for x in bn]
+        return Success(data=bn)
---
+++ hospital/models/config.py
@@ -0,0 +1,18 @@
+"""
+本文件用于后台基本字段设置，包括但不限于关于我们/banner页等内容
+create user: haobin12358
+last update time:2020/3/12 15:39
+"""
+
+from sqlalchemy import Integer, String, Boolean, Text
+from sqlalchemy.dialects.mysql import LONGTEXT
+from hospital.extensions.base_model import Base, Column
+
+class Banner(Base):
+    """banner"""
+    __tablename__ = 'Banner'
+    BNid = Column(String(64), primary_key=True)
+    ADid = Column(String(64), comment='创建者id')
+    BNpicture = Column(Text, nullable=False, comment='图片', url=True)
+    BNsort = Column(Integer, comment='顺序')
+    contentlink = Column(LONGTEXT, comment='跳转链接')
