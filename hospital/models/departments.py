83ff842dc559b99e9b43984bcf8b11b49f3d178c
shuaibinliu <13588046135@163.com>
2020-03-18 02:47:59 +0800

    医生和科室列表增加创建时间字段，增加案例表

--- hospital/models/departments.py
+++ hospital/models/departments.py
@@ -20,12 +20,11 @@ class Departments(Base):
     @orm.reconstructor
     def __init__(self):
         super(Departments, self).__init__()
-        self.fields = ['DEid', 'DEname']
+        self.fields = ['DEid', 'DEname', 'creattime']
 
 
 class Symptom(Base):
     """症状"""
-
     __tablename__ = 'Symptom'
     SYid = Column(String(64), primary_key=True)
     SYname = Column(String(255), nullable=False, comment='症状名')
@@ -51,7 +50,7 @@ class Doctor(Base):
     @orm.reconstructor
     def __init__(self):
         super(Doctor, self).__init__()
-        self.fields = ['DEid', 'DOname', 'DOid', 'DOtitle']
+        self.fields = ['DEid', 'DOname', 'DOid', 'DOtitle', 'createtime']
 
 
 class DoctorMedia(Base):
@@ -62,3 +61,33 @@ class DoctorMedia(Base):
     DMtype = Column(Integer, default=0, comment='0 医生主图 1 医生列表图 2 医生二维码')
     DMmedia = Column(Text, url=True, comment='图片链接')
     DMsort = Column(Integer, default=0, comment='图片顺序')
+
+
+class Example(Base):
+    """案例"""
+    __tablename__ = 'Example'
+    EXid = Column(String(64), primary_key=True)
+    SYid = Column(String(64), comment='症状ID')
+    EXname = Column(String(255), comment='患者姓名')
+    EXgender = Column(Integer, default=0, comment='性别 {0： man，1：woman}')
+    EXage = Column(String(8), comment='年龄')
+    EXheight = Column(String(8), comment='身高')
+    EXweight = Column(String(8), comment='体重')
+    EXaddr = Column(String(256), comment='来历')
+    EXtreated = Column(DateTime, comment='就诊时间')
+    EXchiefComplaint = Column(Text, comment='主诉')
+    EXclinicVisitHistory = Column(Text, comment='就诊史')
+    EXcaseAnalysis = Column(Text, comment='案例分析')
+    EXinspectionResults = Column(Text, comment='校验结果')
+    Exdiagnosis = Column(Text, comment='诊断')
+    EXtreatmentPrinciple = Column(Text, comment='治疗原则')
+    EXtreatmentOutcome = Column(Text, comment='治疗结果')
+    EXperoration = Column(Text, comment='结束语')
+    EXbriefIntroduction = Column(Text, comment='简介')
+    EXmainpic = Column(Text, comment='主图')
+    Exsort = Column(Integer, default=0, comment='排序')
+
+    @orm.reconstructor
+    def __init__(self):
+        super(Example, self).__init__()
+        self.fields = ['EXid', 'EXname', 'SYid', 'createtime', 'EXmainpic']
