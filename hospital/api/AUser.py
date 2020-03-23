3734527a96851ca5f165022f9c431fd13957add6
Wiilz <rayleignze@gmail.com>
2020-03-24 01:31:02 +0800

    user address api

--- hospital/api/AUser.py
+++ hospital/api/AUser.py
@@ -7,10 +7,22 @@ class AUser(Resource):
     def __init__(self):
         self.cuser = CUser()
 
+    def get(self, user):
+        apis = {
+            'provinces': self.cuser.get_provinces,
+            'cities': self.cuser.get_cities,
+            'areas': self.cuser.get_areas,
+            'address_list': self.cuser.address_list,
+            'address': self.cuser.address,
+        }
+        return apis
+
     def post(self, user):
         apis = {
             'mp_login': self.cuser.mini_program_login,
             'test_login': self.cuser.test_login,
+            'set_address': self.cuser.set_address,
+
         }
 
         return apis
