671d07a2c47bb46fb9cb0511d5d48f9dc4cba7d9
Wiilz <rayleignze@gmail.com>
2020-03-26 22:53:22 +0800

    update host config

--- hospital/config/http_config.py
+++ hospital/config/http_config.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-HTTP_HOST = 'https://play.bigxingxing.com'
-API_HOST = 'https://play.bigxingxing.com'
-MEDIA_HOST = 'https://play.bigxingxing.com'
+HTTP_HOST = 'https://hospital.sanbinit.cn'
+API_HOST = 'https://hospital.sanbinit.cn'
+MEDIA_HOST = 'http://image.hospital.sanbinit.cn'
 
-
-# 域名
-PLANET_SERVICE = 'https://play.bigxingxing.com'
-PLANET_SUBSCRIBE = 'https://play.bigxingxing.com'
-PLANET = 'play.bigxingxing.com'
