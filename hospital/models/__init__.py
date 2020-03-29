06d731a1835d77a84ba1299e6f008fcc80e7f562
shuaibinliu <13588046135@163.com>
2020-03-29 23:32:14 +0800

    conflict resolution'

--- hospital/models/__init__.py
+++ hospital/models/__init__.py
@@ -4,9 +4,6 @@ from .admin import *
 from .user import *
 from .classes import *
 from .video import *
-<<<<<<< HEAD
 from .register import *
-=======
 from .evaluation import *
 from .assistance import *
->>>>>>> 2396a5663efa4419b8ccd7c5d4473f1970b8a040
