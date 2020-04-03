811f22940219be25bb5f81791615b99210dcc44b
haobin12358 <1276121237@qq.com>
2020-04-04 03:41:26 +0800

    task

--- hospital/api/AConfig.py
+++ hospital/api/AConfig.py
@@ -11,7 +11,9 @@ class AConfig(Resource):
             "list_banner": self.cconfig.list_banner,
             "get_csd": self.cconfig.get_csd,
             "get_about_us": self.cconfig.get_about_us,
-            "get_vip_price": self.cconfig.get_vip_price
+            "get_vip_price": self.cconfig.get_vip_price,
+            "get_pointtask": self.cconfig.get_pointtask,
+            "get_integral": self.cconfig.get_integral
         }
 
         return apis
@@ -23,7 +25,9 @@ class AConfig(Resource):
             "set_about_us": self.cconfig.set_about_us,
             "set_characteristic_team": self.cconfig.set_characteristic_team,
             "set_honour": self.cconfig.set_honour,
-            "set_vip_price": self.cconfig.set_vip_price
+            "set_vip_price": self.cconfig.set_vip_price,
+            "update_pointtask": self.cconfig.update_pointtask,
+            "get_point": self.cconfig.get_point
         }
 
         return apis
