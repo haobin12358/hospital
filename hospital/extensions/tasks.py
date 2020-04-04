9d4f130e5265e729c95640920983cb28ffeb29a9
Wiilz <rayleignze@gmail.com>
2020-04-04 18:01:44 +0800

    feat(tasks): add async task common function

--- hospital/extensions/tasks.py
+++ hospital/extensions/tasks.py
@@ -1,10 +1,37 @@
 # -*- coding: utf-8 -*-
 from flask import current_app
 from sqlalchemy import false
-
+from datetime import timedelta
 from hospital.config.enums import ActivityStatus, UserActivityStatus
-from hospital.extensions.register_ext import celery, db
+from hospital.extensions.register_ext import celery, db, conn
 from hospital.models import Activity, UserActivity
+
+
+def add_async_task(func, start_time, func_args, conn_id=None):
+    """
+    添加异步任务
+    func: 任务方法名 function
+    start_time: 任务执行时间 datetime
+    func_args: 函数所需参数 tuple
+    conn_id: 要存入redis的key
+    """
+    task_id = func.apply_async(args=func_args, eta=start_time - timedelta(hours=8))
+    connid = conn_id if conn_id else str(func_args[0])
+    current_app.logger.info('add async task: func_args:{} | connid: {}, task_id: {}'.format(func_args, connid, task_id))
+    conn.set(connid, str(task_id))
+
+
+def cancel_async_task(conn_id):
+    """
+    取消已存在的异步任务
+    conn_id: 存在于redis的key
+    """
+    exist_task_id = conn.get(conn_id)
+    if exist_task_id:
+        exist_task_id = str(exist_task_id, encoding='utf-8')
+        celery.AsyncResult(exist_task_id).revoke()
+        conn.delete(conn_id)
+        current_app.logger.info('取消任务成功 task_id:{}'.format(exist_task_id))
 
 
 @celery.task()
